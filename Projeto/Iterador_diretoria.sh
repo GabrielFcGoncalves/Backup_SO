7d1bcd084778d61da22d257b81030766820f76be
GabrielFcGoncalves <gabfacgon3@gmail.com>
2024-10-17 11:43:59 +0100

    Sem Comentários

--- Projeto/Iterador_diretoria.sh
+++ Projeto/Iterador_diretoria.sh
@@ -5,41 +5,32 @@ function iterador_diretoria(){
     path_diretoria_destino="$2"
     prefixo="$3"
 
+  
     for file in "$diretoria_atual"/*; do
         
-        path_original_file="${file}"
-
-        aux=${file#"$prefixo"}
-
-        path_backup_file="${path_diretoria_destino}${aux}"
-        echo "Ficheiro --------------__> $file"
-
-        # if [ -e "$path_original_file" ]; then
-        #         echo "Tudo ok"
-        #     else
-        #         echo "------------------------------------File '$ficheiro_backup' does not exist in '$path_original_file'."
-        # fi
-
         
-        if [ -f "$file" ]; then
-            cp -a "$file" "$path_backup_file"
-        fi
+            path_original_file="${file}"
 
-        if [ -d "$file" ]; then
+            path_backup_file="${path_diretoria_destino}${file#"$prefixo"}"
 
-            # for ficheiro_backup in "$path_backup_file"/*; do
-              
-            #     if [ -e "$path_original_file/$(basename "$ficheiro_backup")" ]; then
-            #         echo "Tudo ok"
-            #     else
-            #         echo "------------------------------------File '$ficheiro_backup' does not exist in '$path_original_file'."
-            #     fi
 
-            # done
-            mkdir "$path_backup_file"
-            ./Iterador_diretoria.sh $file $path_diretoria_destino $prefixo
-        fi
-        
+            
+            if [ -f "$file" ]; then
+                cp -a "$file" "$path_backup_file"
+            fi
+
+            if [ -d "$file" ]; then
+
+                if [ ! -d "$path_backup_file" ]; then
+                    mkdir "$path_backup_file"
+            
+                fi
+                
+                ./Iterador_diretoria.sh $file $path_diretoria_destino $prefixo   
+                
+                
+            fi
+            
 
     done
 
