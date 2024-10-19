ed154503020a196561fce593e78395a58fd86f6d
sle3pyy <simaomarquesantos@gmail.com>
2024-10-19 20:14:22 +0100

    backup so funciona pra caminhos inteiros e backup_files ja faz oq devia

--- Projeto/Backup_files.sh
+++ Projeto/Backup_files.sh
@@ -1,51 +1,21 @@
-        
+
 function Backup_files(){
+    diretoria_incial=$(echo $1 | cut -d'/' -f1)
     diretoria_atual="$1"
     path_diretoria_destino="$2"
 
     for file in "$diretoria_atual"/*; do
-
         
-        if [ -f "$file" ]; then
+            path_original_file="${file}"
 
-            if [ ! -e "$path_diretoria_destino" ]; then
-                echo "File $file is new. Backing up." 
-                cp -a "$file" "$path_diretoria_destino"
+            path_backup_file="${path_diretoria_destino}/$(basename "$file")"
 
-            else
-
-                md5_source=$(md5sum "$file" | awk '{ print $1 }')
-                md5_backup=$(md5sum "$path_diretoria_destino/$(basename $file)" | awk '{ print $1 }')
-
-                if [ "$md5_source" != "$md5_backup" ]; then
-                        echo "File $(basename $file) has been updated. Backing up now."
-                        cp -a "$file" "$path_diretoria_destino"
-                fi
+            echo $file
+            
+            if [ -f "$file" ]; then
+                cp -a "$file" "$path_backup_file"
             fi
-        fi
-
-
     done
 }
 
-
-function main(){
-    
-    diretoria_a_copiar="$1"
-    path_diretoria_destino="$2/$(basename "$1")_backup"
-  
-   if [ ! -e "$path_diretoria_destino" ]; then
-        echo "$backup does not exist."
-        mkdir "$path_diretoria_destino"
-        Backup_files "$diretoria_a_copiar" "$path_diretoria_destino"
-        echo "$backup has been created"
-    else
-        echo "$backup already exists."
-        Backup_files "$diretoria_a_copiar" "$path_diretoria_destino"
-    fi
-    
-
-}
-
-
-main $1 $2
+Backup_files $1 $2
--- Projeto/backup.sh
+++ Projeto/backup.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-
 function iterador_diretoria(){
     diretoria_atual="$1"
     path_diretoria_destino="$2"
 
-    echo "$1 CARALHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"   
-
     for file in "$diretoria_atual"/*; do
-
-            path_original_file="$file"
+        path_original_file="$file"
+        relative_path="${path_original_file#$starting_dir/}"
+        path_backup_file="$path_diretoria_destino/$relative_path"
         
-            relative_path="${path_original_file#$starting_dir/}"  
-            path_backup_file="$path_diretoria_destino/$relative_path"
-            
-            backup_dir=$(dirname "$path_backup_file")  
-            mkdir -p "$backup_dir"  
+        backup_dir=$(dirname "$path_backup_file")
+        mkdir -p "$backup_dir"
 
-            echo $backup_dir
-            echo $file
-            
-            if [ -f "$file" ]; then
-                cp -a "$file" "$path_backup_file"
-            elif [ -d "$file" ]; then
-                iterador_diretoria "$file" "$path_diretoria_destino"  
-            fi
+        if [ -f "$file" ]; then
+            cp -a "$file" "$path_backup_file"
+            echo "Copied file: $file to $path_backup_file"
+        elif [ -d "$file" ]; then
+            echo "Entering directory: $file"
+            iterador_diretoria "$file" "$path_diretoria_destino"
+        fi
     done
 }
-
 
 function main(){
     
@@ -38,8 +31,8 @@ function main(){
    if [ ! -e "$path_diretoria_destino" ]; then
         echo "$backup does not exist."
         mkdir "$path_diretoria_destino"
-        iterador_diretoria "$diretoria_a_copiar" "$path_diretoria_destino"
         echo "$backup has been created"
+        iterador_diretoria "$diretoria_a_copiar" "$path_diretoria_destino"
     else
         echo "$backup already exists."
         ./Iterador_Backup.sh "$diretoria_a_copiar" "$path_diretoria_destino"
@@ -50,5 +43,4 @@ function main(){
 
 
 starting_dir=$1
-echo $starting_dir
 main $1 $2
