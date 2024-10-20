1f40053b660f3f7714643de2e5dd4effed59ab11
GabrielFcGoncalves <gabfacgon3@gmail.com>
2024-10-20 19:09:07 +0100

    Backup_files a funfar 100% - Desta vez é verdade

--- Projeto/Backup_files.sh
+++ Projeto/Backup_files.sh
@@ -1,28 +1,50 @@
-
+        
 function Backup_files(){
-    diretoria_incial=$(echo $1 | cut -d'/' -f1)
     diretoria_atual="$1"
     path_diretoria_destino="$2"
 
     for file in "$diretoria_atual"/*; do
-        
-<<<<<<< Updated upstream
-            path_original_file="${file}"
 
-            path_backup_file="${path_diretoria_destino}/$(basename "$file")"
-=======
+        
         if [ -f "$file" ]; then
             if [ ! -e "$path_diretoria_destino/$(basename $file)" ]; then
                 echo "File $file is new. Backing up." 
                 cp -a "$file" "$path_diretoria_destino"
->>>>>>> Stashed changes
 
-            echo $file
-            
-            if [ -f "$file" ]; then
-                cp -a "$file" "$path_backup_file"
+            else
+
+                md5_source=$(md5sum "$file" | awk '{ print $1 }')
+                md5_backup=$(md5sum "$path_diretoria_destino/$(basename $file)" | awk '{ print $1 }')
+
+                if [ "$md5_source" != "$md5_backup" ]; then
+                        echo "File $(basename $file) has been updated. Backing up now."
+                        cp -a "$file" "$path_diretoria_destino"
+                fi
             fi
+        fi
+
+
     done
 }
 
-Backup_files $1 $2
+
+function main(){
+    
+    diretoria_a_copiar="$1"
+    path_diretoria_destino="$2/$(basename "$1")_backup"
+  
+   if [ ! -e "$path_diretoria_destino" ]; then
+        echo "$backup does not exist."
+        mkdir "$path_diretoria_destino"
+        Backup_files "$diretoria_a_copiar" "$path_diretoria_destino"
+        echo "$backup has been created"
+    else
+        echo "$backup already exists."
+        Backup_files "$diretoria_a_copiar" "$path_diretoria_destino"
+    fi
+    
+
+}
+
+
+main $1 $2
