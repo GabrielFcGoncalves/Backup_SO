c57e298f80db0faeaf525660ee2a8af6480e20ba
sle3pyy <simaomarquesantos@gmail.com>
2024-10-24 13:02:55 +0100

    rm files and bunch of stuff

--- Projeto/belito_backup.sh
+++ Projeto/belito_backup.sh
@@ -12,7 +12,10 @@ function check_dir_integ(){
     fi
 }
 
-# Function to read exclusion list from a file
+execute() {
+    [ "$flag_c" = "false" ] && eval "${@}" && echo "Command executed successfully: ${@}" || echo "${@}"
+}
+
 function read_exclusion_list() {
     local exclusion_file="$1"
     exclusion_list=()
@@ -24,7 +27,6 @@ function read_exclusion_list() {
     done < "$exclusion_file"
 }
 
-# Function to check if a file or directory is in the exclusion list
 function is_excluded() {
     local path="$1"
     for excluded in "${exclusion_list[@]}"; do
@@ -46,6 +48,7 @@ function backup_gen(){
             0)
                 relative_path="${file#$starting_dir/}"
                 path_backup_file="$path_diretoria_destino/$relative_path"
+                
                 backup_dir=$(dirname "$path_backup_file")
 
                 if is_excluded "$file"; then
@@ -54,72 +57,77 @@ function backup_gen(){
                 fi
 
                 if [ ! -e "$backup_dir" ]; then
-                    mkdir -p "$backup_dir"
+                    execute mkdir -p "$backup_dir"
                 fi
 
                 if [ -f "$file" ]; then
-                    cp -a "$file" "$path_backup_file"
-                    echo "Copied file: $file to $path_backup_file"
+                    if $flag_r; then
+                        if [[ ! "$file" =~ $expression ]]; then
+                            echo "Skipping $file: doesnt match the provided expression. "
+                            continue
+                        fi
+                    fi
+                    if [ ! -e "$path_backup_file" ]; then 
+                        execute cp -a "$file" "$path_backup_file"
+                    fi    
+
                 elif [ -d "$file" ]; then
-                    echo "Entering directory: $file"
+                    #echo "Entering directory: $file"
                     backup_gen "$file" "$path_diretoria_destino" "$mode"
                 fi
                 ;;
             
             1)
                 relative_path="${file#$end_dir/$dir_backup}"
-                path_backup_file="$path_diretoria_destino$relative_path"
+                path_original_path="$path_diretoria_destino$relative_path"
+
+                if is_excluded "$path_original_path"; then
+                    echo "Removing $file from the backup as it is in the provided exclusion file."
+                    if [ -f $file ]; then
+                        execute rm $file
+                    else 
+                        execute rm -r $file
+                    fi
+                    continue
+                fi
 
                 if [ -f "$file" ]; then
 
-                    if [ ! -e $path_backup_file ]; then
-                        rm $file;
+                    if $flag_r; then
+                        if [[ ! "$file" =~ $expression ]]; then
+                            echo "Deleting $file: doesnt match the provided expression. "
+                            execute rm $file
+                            continue
+                        fi
+                    fi
+
+                    if [ ! -e $path_original_path ]; then
+                        execute rm $file;
                     else
                         md5_source=$(md5sum "$file" | awk '{ print $1 }')
-                        md5_backup=$(md5sum "$path_backup_file" | awk '{ print $1 }')
+                        md5_backup=$(md5sum "$path_original_path" | awk '{ print $1 }')
 
                         if [ "$md5_source" != "$md5_backup" ]; then
                             echo "File $(basename $file) has been updated."                        
-                            echo "cp -a" "$path_backup_file" "$file"
+                            execute "cp -a" "$path_original_path" "$file"
                         fi
                     fi
 
                 elif [ -d "$file" ]; then
-                    echo "Entering directory: $file"
-                    backup_gen "$file" "$path_diretoria_destino" "$mode"
+                    if [ ! -e "$path_original_path" ]; then
+                        echo "Dir $path_original_path has been deleted. Skipping."
+                        execute rm -r $file
+                        continue
+                    else
+                        #echo "Entering directory: $file"
+                        backup_gen "$file" "$path_diretoria_destino" "$mode"
+                    fi
                 fi
 
                 ;;
                 
 
         esac
-    done
-}
-
-function backup_gen_c(){
-    diretoria_atual="$1"
-    path_diretoria_destino="$2"
-
-    for file in "$diretoria_atual"/*; do
-        relative_path="${file#$starting_dir/}"
-        path_backup_file="$path_diretoria_destino/$relative_path"
-        
-        backup_dir=$(dirname "$path_backup_file")
-
-        if is_excluded "$file"; then
-            echo "Skipping excluded file or directory: $file"
-            continue
-        fi
-
-        if [ ! -e "$backup_dir" ]; then
-            echo "mkdir -p" "$backup_dir"
-        fi
-
-        if [ -f "$file" ]; then
-            echo "cp -a" "$file" "$path_backup_file"
-        elif [ -d "$file" ]; then
-            backup_gen_c "$file" "$path_diretoria_destino"
-        fi
     done
 }
 
@@ -138,44 +146,34 @@ function main(){
 
     dir_backup="$(basename "$starting_dir")_backup"
 
-    path_diretoria_destino="$end_dir/${dir_backup}"
+    path_diretoria_destino="$end_dir/${dir_backup}"    
 
-    echo "UAAAAAAAAAAAAAAAA ---------------> $path_diretoria_destino"
-    
-
-    if $check_flag_b; then
+    if $flag_b; then
         echo "-b enabled with file $file_txt."
         read_exclusion_list "$file_txt"
     fi
 
-    if $check_flag_c; then
-        echo "-c enabled."
-        backup_gen_c "$starting_dir" "$path_diretoria_destino"
+    if [ ! -e "$path_diretoria_destino" ]; then
+        echo "$path_diretoria_destino does not exist. Creating it."
+        execute mkdir -p "$path_diretoria_destino"
     fi
-
-
-    if ! $check_flag_c; then
-        if [ ! -e "$path_diretoria_destino" ]; then
-            echo "$path_diretoria_destino does not exist. Creating it."
-            mkdir -p "$path_diretoria_destino"
-        fi
-        backup_gen "$starting_dir" "$path_diretoria_destino" "0"
-        backup_gen  "$path_diretoria_destino" "$starting_dir" "1"
-    fi
+    backup_gen "$starting_dir" "$path_diretoria_destino" "0"
+    backup_gen  "$path_diretoria_destino" "$starting_dir" "1"
 }
 
-check_flag_c=false
-check_flag_b=false
-check_flag_r=false
+flag_c=false
+flag_b=false
+flag_r=false
 file_txt=""
+expression=""
 
-while getopts ":cb:" opt; do
+while getopts ":cb:r:" opt; do
     case $opt in
         c)
-            check_flag_c=true
+            flag_c=true
             ;;
         b)
-            check_flag_b=true
+            flag_b=true
             file_txt="$OPTARG"
             if [ ! -f "$file_txt" ]; then
                 echo "Error: Backup file '$file_txt' not found."
@@ -186,7 +184,7 @@ while getopts ":cb:" opt; do
             fi
             ;;
         r)
-            check_flag_r=true
+            flag_r=true
             expression="$OPTARG"
             ;;
         \?)
